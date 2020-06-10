5736044db1db4691b864365f80a4e6aa8f2b927d
jiaoyuan <jiaoyuan@codeaurora.org>
2020-06-10 16:04:09 +0800

    Snapcam: catch IllegalArgumentException
    catch IllegalArgumentException
    
    Change-Id: Id9d44f18f247ce3f0ab8d6b49ab130f002370692

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -6805,7 +6805,7 @@ public class CaptureModule implements CameraModule, PhotoController,
                             mCaptureCallback, mCameraHandler);
                 }
             }
-        } catch (CameraAccessException | IllegalStateException | NullPointerException e) {
+        } catch (CameraAccessException | IllegalStateException | NullPointerException | IllegalArgumentException e) {
             e.printStackTrace();
         }
     }
