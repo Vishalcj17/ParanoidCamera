Kunal446/snap

Snapcam: add mctf tag
add mctf tag

Change-Id: Ic320083897d8d30a247de301a26e362e783101a7

src/com/android/camera/CaptureModule.java
@@ -473,6 +473,8 @@ public class CaptureModule implements CameraModule, PhotoController,
             new CaptureRequest.Key<>("org.quic.camera.recording.endOfStream", byte.class);
     public static final CaptureRequest.Key<Integer> earlyPCR =
             new CaptureRequest.Key<>("org.codeaurora.qcamera3.sessionParameters.numPCRsBeforeStreamOn", Integer.class);
+    public static final CaptureRequest.Key<Byte> mctf =
+            new CaptureRequest.Key<>("org.codeaurora.qcamera3.sessionParameters.enableMCTFwithReferenceFrame", byte.class);
     private static final CaptureResult.Key<Byte> is_depth_focus =
             new CaptureResult.Key<>("org.quic.camera.isDepthFocus.isDepthFocus", byte.class);
     private static final CaptureRequest.Key<Byte> capture_burst_fps =
@@ -512,6 +514,8 @@ public class CaptureModule implements CameraModule, PhotoController,
             new CaptureResult.Key<>("org.quic.camera2.objectTrackingResults.TrackerScore", Integer.class);
     public static final CameraCharacteristics.Key<Integer> mfnr_type =
             new CameraCharacteristics.Key<>("org.quic.camera.swcapabilities.MFNRType", Integer.class);
+    public static final CameraCharacteristics.Key<Byte> swmctf =
+            new CameraCharacteristics.Key<>("org.quic.camera.swcapabilities.SWMCTFEnable", byte.class);
 
     private static final CaptureRequest.Key<Byte> rawinfo_idealraw_request =
             new CaptureRequest.Key<>("com.qti.chi.rawcbinfo.IdealRaw", byte.class);
@@ -2171,6 +2175,15 @@ public class CaptureModule implements CameraModule, PhotoController,
             setUpVideoCaptureRequestBuilder(mVideoRecordRequestBuilder, cameraId);
             mPreviewRequestBuilder[cameraId] = mVideoRecordRequestBuilder;
             mIsPreviewingVideo = true;
+            //add for mctf tag
+            if(mSettingsManager.isSwMctfSupported()){
+                int mctfVaule = SystemProperties.getInt("persist.sys.camera.sessionParameters.mctf", 0);
+                try {
+                    mVideoRecordRequestBuilder.set(CaptureModule.mctf, (byte)(mctfVaule == 1 ? 0x01 : 0x00));
+                } catch (IllegalArgumentException e) {
+                    Log.d(TAG, "mctf no vendor tag");
+                }
+            }
             if (ApiHelper.isAndroidPOrHigher()) {
                 if (isHighSpeedRateCapture()) {
                     int optionMode = isSSMEnabled() ? STREAM_CONFIG_SSM : SESSION_HIGH_SPEED;

Vishalcj17/ParanoidCamera

src/com/android/camera/CaptureModule.java
@@ -526,6 +526,8 @@ public class CaptureModule implements CameraModule, PhotoController,
     public static final CaptureRequest.Key<Integer> extendedMaxZoom =
             new CaptureRequest.Key<>("org.codeaurora.qcamera3.sessionParameters.ExtendedMaxZoom", Integer.class);
 
+    public static final CaptureRequest.Key<Byte> mctf =
+            new CaptureRequest.Key<>("org.codeaurora.qcamera3.sessionParameters.enableMCTFwithReferenceFrame", byte.class);
     private static final CaptureResult.Key<Byte> is_depth_focus =
             new CaptureResult.Key<>("org.quic.camera.isDepthFocus.isDepthFocus", byte.class);
     private static final CaptureRequest.Key<Byte> capture_burst_fps =
@@ -573,6 +575,8 @@ public class CaptureModule implements CameraModule, PhotoController,
     public static CaptureResult.Key<Integer> multiframe_burst_enable =
             new CaptureResult.Key<>("org.quic.camera.MultiFrame.MultiframeBurstEnable", Integer.class);
 
+    public static final CameraCharacteristics.Key<Byte> swmctf =
+            new CameraCharacteristics.Key<>("org.quic.camera.swcapabilities.SWMCTFEnable", byte.class);
 
     private TouchTrackFocusRenderer mT2TFocusRenderer;
     private StateNNTrackFocusRenderer mStateNNFocusRenderer;
@@ -4646,8 +4650,20 @@ public class CaptureModule implements CameraModule, PhotoController,
         }
         applyFaceContourVersion(builder);
         applyExtendMaxZoom(builder);
+        applyMctf(builder);
     }
 
+    private void applyMctf(CaptureRequest.Builder builder){
+        //add for mctf tag
+        if(mSettingsManager.isSwMctfSupported() && (mCurrentSceneMode.mode == CameraMode.VIDEO || mCurrentSceneMode.mode == CameraMode.HFR)){
+            int mctfVaule = PersistUtil.mctfValue();
+            try {
+                builder.set(CaptureModule.mctf, (byte)(mctfVaule == 1 ? 0x01 : 0x00));
+            } catch (IllegalArgumentException e) {
+                Log.d(TAG, "mctf no vendor tag");
+            }
+        }
+    }
     private void applyCommonSettings(CaptureRequest.Builder builder, int id) {
         builder.set(CaptureRequest.CONTROL_MODE, CaptureRequest.CONTROL_MODE_AUTO);
         builder.set(CaptureRequest.CONTROL_AF_MODE, mControlAFMode);